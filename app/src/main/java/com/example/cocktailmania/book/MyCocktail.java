1d34b3e22cd2f7b61b737f13ebb6616636116187
menoz <menozzi2000@gmail.com>
2021-08-05 01:55:48 +0200

    mycocktail

---
+++ app/src/main/java/com/example/cocktailmania/book/MyCocktail.java
@@ -0,0 +1,16 @@
+package com.example.cocktailmania.book;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.cocktailmania.R;
+
+public class MyCocktail extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_my_cocktail);
+    }
+}
