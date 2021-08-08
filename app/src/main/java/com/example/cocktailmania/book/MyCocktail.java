2a5f23c1c8bb14080b2b825299dcf2853a8ce443
menoz <menozzi2000@gmail.com>
2021-08-08 16:45:13 +0200

    select img and insert name

--- app/src/main/java/com/example/cocktailmania/book/MyCocktail.java
+++ app/src/main/java/com/example/cocktailmania/book/MyCocktail.java
@@ -1,30 +1,62 @@
 package com.example.cocktailmania.book;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
 
 import com.example.cocktailmania.R;
 import com.example.cocktailmania.cocktail.CocktailActivity;
 
-public class MyCocktail extends AppCompatActivity {
+public class MyCocktail extends AppCompatActivity implements View.OnClickListener{
+
+    private static final int RESULT_LOAD_IMAGE = 1;
+    ImageView imgToUpload;
+    Button uploadButton;
+    EditText uploadCktName;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_my_cocktail);
 
-        //provvisorio, da controllare
-        Button addButton=findViewById(R.id.add_myckt);
-        addButton.setOnClickListener(v -> openAddCkt());
+        imgToUpload = (ImageView) findViewById(R.id.imgUpload);
+        uploadButton = (Button) findViewById(R.id.uploadButton);
+        uploadCktName = (EditText) findViewById(R.id.cktName);
 
+        imgToUpload.setOnClickListener(this);
+        uploadButton.setOnClickListener(this);
     }
 
-    private void openAddCkt(){
-        Intent intent = new Intent(this, addCocktail.class);
-        startActivity(intent);
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.imgUpload:
+                Intent galleryIntent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+                startActivityForResult(galleryIntent, RESULT_LOAD_IMAGE);
+                break;
+            case R.id.uploadButton:
+
+                break;
+            default:
+                break;
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == RESULT_LOAD_IMAGE && resultCode == RESULT_OK && data != null) {
+            Uri selectedImage=data.getData();
+            imgToUpload.setImageURI(selectedImage);
+        }
     }
 }
 
