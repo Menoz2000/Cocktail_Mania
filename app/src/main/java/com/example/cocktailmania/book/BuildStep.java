5050fb4ad7f957f5c9f47e9d47069ad26bcd7072
Andrea Soncini <277224@studenti.unimore.it>
2021-08-26 14:39:36 +0200

    Create xml for my cocktail part

---
+++ app/src/main/java/com/example/cocktailmania/book/BuildStep.java
@@ -0,0 +1,15 @@
+package com.example.cocktailmania.book;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.cocktailmania.R;
+
+public class BuildStep extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_build_step);
+    }
+}
